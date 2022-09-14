cadda7d48da0b4ffb9751768166d44bb6abbcc8d
Gizem Izmir <gizkes1996@gmail.com>
2022-09-14 10:28:23 +0300

    playlistSlice and searchSlice created.

--- src/store/index.js
+++ src/store/index.js
@@ -18,9 +18,44 @@ const themeSlice = createSlice({
   },
 });
 
+const playlistSlice = createSlice({
+  name: "playlists",
+  initialState: {
+    playlistItems: [],
+  },
+  reducers: {
+    setPlaylists: (state, action) => {
+      const { playlists } = action.payload;
+      return {
+        playlistItems: playlists,
+      };
+    },
+  },
+});
+
+const searchSlice = createSlice({
+    name: 'search',
+    initialState: {
+      searchTracks: [],
+    },
+    reducers: {
+        setSearchTracks: (state, action) => {
+        const {search} = action.payload;
+        return {
+            searchTracks: search,
+        };
+      },
+    },
+  });
+
 export const { toggleTheme } = themeSlice.actions;
+export const { setPlaylists } = playlistSlice.actions;
+export const {setSearchTracks} = searchSlice.actions;
+
 export const store = configureStore({
   reducer: combineReducers({
     theme: themeSlice.reducer,
+    playlists: playlistSlice.reducer,
+    search: searchSlice.reducer,
   }),
 });
