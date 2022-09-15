df41118780c7695103bbec0d7d0ceafd66b2257e
Gizem Izmir <gizkes1996@gmail.com>
2022-09-15 11:51:10 +0300

    Setting and Profile screens updated

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,16 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import MusicItem from "../components/MusicItem";
 
 const Profile = () => {
-    const {navigate} = useNavigation();
+  const likes = useSelector((state) => state.likes.likeItems);
+  const theme = useSelector((state) => state.theme.activeTheme);
+
+  const renderMusicItem = ({ item }) => {
+    return <MusicItem music={item} />;
+  };
 
   return (
-    <View>
-      <Text>Profile</Text>
-      <Text onPress={() => navigate('Settings')}>Go to Settings</Text>
+    <View
+      style={[
+        styles.musicContainer,
+        { backgroundColor: theme.backgroundColor },
+      ]}
+    >
+      <Text style={[styles.pageTitle, { color: theme.color }]}>
+        Liked Songs
+      </Text>
+      {Object.keys(likes).length > 0 ? (
+        <FlatList
+          data={likes}
+          renderItem={renderMusicItem}
+          keyExtractor={(item, index) => `music-${item.id}`}
+        />
+      ) : (
+        <Text style={[styles.profilePageText, { color: theme.color }]}>
+          Couldn't Find Any Likes
+        </Text>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+const styles = StyleSheet.create({
+  musicContainer: {
+    flex: 1,
+    padding: 30,
+  },
+  pageTitle: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  profilePageText: {
+    display: "flex",
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: "50%",
+  },
+});
+
+export default Profile;
--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,16 +1,90 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import React from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+
+import { logOut } from "../store";
+import { auth } from "../utils/firebase";
 
 const Settings = () => {
-    const {navigate} = useNavigation();
+  const { navigate } = useNavigation();
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.activeTheme);
+
+  const handleLogout = async () => {
+    // Remove user from AsyncStorage
+    dispatch(logOut());
+    auth.signOut()
+  };
 
   return (
-    <View>
-      <Text>Settings</Text>
-      <Text onPress={() => navigate('ThemeSettingsScreen')}>Go to Theme</Text>
+    <View
+      style={[
+        styles.settingContainer,
+        { backgroundColor: theme?.backgroundColor },
+      ]}
+    >
+      <Image
+        style={styles.profileImage}
+        source={{ uri: "https://i.pravatar.cc/100?img=2" }}
+      />
+      <Pressable
+        style={styles.settingButton}
+        onPress={() => {
+          navigate("ThemeSettingsScreen");
+        }}
+      >
+        <Text style={styles.buttonText}>Theme</Text>
+      </Pressable>
+      <Pressable
+        style={styles.settingButton}
+        onPress={() => {
+          navigate("ProfileSettingsScreen");
+        }}
+      >
+        <Text style={styles.buttonText}>Edit Profile</Text>
+      </Pressable>
+      <Pressable style={[styles.settingButton]} onPress={handleLogout}>
+        <Text style={styles.buttonText}>Log out</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default Settings
+const styles = StyleSheet.create({
+  settingContainer: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 150,
+    marginBottom: 30,
+  },
+  settingButton: {
+    width: 200,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#2196F3",
+    marginBottom: 30,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 15,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+  },
+});
+
+export default Settings;
